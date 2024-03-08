004bdbabca64878d5321a7d7b6478235b001cdc0
aflorian <florian.albora@ynov.com>
2024-03-08 16:56:01 +0100

    add likes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/*.{html,js}"],
+  content: ["./src/pages/*.{html,js,ejs}"],
   theme: {
     extend: {},
   },
